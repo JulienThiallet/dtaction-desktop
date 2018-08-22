b0a0720fe42c709406cafcb9dec2e001f318a04d
Julien THIALLET <julien.thiallet@gmail.com>
2018-08-22 17:22:24 +0200

    Create Home + nav + classes

---
+++ Dtaction-desktop/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dtaction_desktop
+{
+    public class User
+    {
+        public static int Id { get; set; }
+        public static string Username { get; set; }
+        public static string Email { get; set; }
+        public static string Password { get; set; }
+        public static string Image { get; set; }
+    }
+}
